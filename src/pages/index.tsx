1386985dcf004f86a7e5de5afb1cab504726f40d
yousefh409 <yousefh409@gmail.com>
2022-08-26 00:29:08 -0700

    style stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
 import * as React from "react"
 import homeworkData from "../data/homeworkData.json"
+import * as indexStyles from "../styles/indexStyles.module.css"
 
 export default class Home extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className={indexStyles.indexWrapper}>
+        <div className={indexStyles.title}>
           EE120 Self-Grade Form
         </div>
 
-        <div>
+        <div className={indexStyles.description}>
           Please select which homework you would like to self-grade from below:
         </div>
 
