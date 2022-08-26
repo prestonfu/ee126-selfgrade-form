985b3c3cfb9430d8fa3457d08b72b01b121b7367
yousefh409 <yousefh409@gmail.com>
2022-08-26 10:34:41 -0700

    homework table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
+import { Link } from "gatsby"
 import homeworkData from "../data/homeworkData.json"
 import * as indexStyles from "../styles/indexStyles.module.css"
+import ExternalLink from "../components/externalLink"
 
 export default class Home extends React.Component {
   render() {
@@ -15,10 +17,34 @@ export default class Home extends React.Component {
         </div>
 
         <div>
-          {homeworkData.map(homework => (
-              <div>{homework.name}</div>
-            ))
-          }
+        <table className={`table table-hover table-primary ${indexStyles.homeworkTable}`}>
+          <thead style={{ border: "none" }}>
+            <th>Homework</th>
+            <th>Gradescope</th>
+            <th>Solutions</th>
+          </thead>
+          <tbody>
+            {homeworkData.map((homework) =>
+              <tr>
+                <td>
+                  <Link to={"/" + homework.pageName}>
+                    {homework.name}
+                  </Link>
+                </td>
+                <td>
+                  <ExternalLink href={homework.gradescopeLink}>
+                      Gradescope
+                  </ExternalLink>
+                </td>
+                <td>
+                  <ExternalLink href={homework.solutionsLink}>
+                      Solutions
+                  </ExternalLink>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
         </div>
       </div>
     )
